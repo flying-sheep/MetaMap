1036effe01c2d9997f163950c1ecc19c50c6a82c
george <gtsitsiridis@gmail.com>
2018-01-11 12:47:54 +0100

    test

--- R/global.r
+++ R/global.r
@@ -51,6 +51,6 @@ pkg_file <- function(path = '.') {
 DIR <- ifelse(is_in_package(), "R/data", "data")
 MAX_SAMPLES <- 150
 
-print(file.path(DIR, 'study_info.RData'))
+print(system.file("R", package = 'metatranscriptome'))
 
 load(pkg_file(file.path(DIR, 'study_info.RData')))
