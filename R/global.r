b1bd69a99b9f54b91a39076691fa0e6e8df93d30
george <gtsitsiridis@gmail.com>
2018-01-15 16:25:33 +0100

    test

--- R/global.r
+++ R/global.r
@@ -56,6 +56,7 @@ print(getwd())
 print(list.files("R"))
 print(list.files("R/data"))
 print(environment(is_in_package))
+print(pkg_file)
 print(pkg_file(file.path(DIR, 'studies')))
 STUDIES <- list.files(pkg_file(file.path(DIR, 'studies'))) %>%
   str_split_fixed("\\.", n = 2) %>% .[,1]
