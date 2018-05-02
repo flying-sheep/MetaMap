49caf5a1a4bf4008eab35cfdbf7efe9fba167477
george <gtsitsiridis@gmail.com>
2018-05-02 22:03:10 +0200

    sankey size

--- R/server.r
+++ R/server.r
@@ -241,8 +241,7 @@ server <-
 
       # load phylo from .RData file
       cls <-
-        class(try(loadPhylo(study, DIR, environment()))
-        )
+        class(try(loadPhylo(study, DIR, environment())))
       if (cls == "try-error")
       {
         values$phylo <- NULL
@@ -459,7 +458,7 @@ server <-
         return(DT::datatable(data.frame(Samples = "Empty"), selection = "none"))
       }
       sam_data <-
-        values$phylo@sam_data[,-which(values$phylo@sam_data %>% colnames == "All")]
+        values$phylo@sam_data[, -which(values$phylo@sam_data %>% colnames == "All")]
       DT::datatable(
         data.frame(sam_data),
         options = list(
@@ -842,11 +841,11 @@ server <-
       if (length(selected_rows) != 0) {
         de_table <-
           if (event$curveNumber == 0)
-            de_table[-selected_rows, ]
+            de_table[-selected_rows,]
         else
-          de_table[selected_rows, ]
+          de_table[selected_rows,]
       }
-      species <- de_table[event$pointNumber + 1, ]
+      species <- de_table[event$pointNumber + 1,]
       values$species_diff <-
         unique(c(values$species_diff, rownames(species)))
       updateSelectInput(
@@ -872,7 +871,7 @@ server <-
       ))
         return(NULL)
       # print(values$species_diff)
-      de_table_subset <- values$de_table[values$species_diff, ]
+      de_table_subset <- values$de_table[values$species_diff,]
       DT::datatable(
         de_table_subset %>% as.data.frame,
         options = list(
@@ -931,10 +930,12 @@ server <-
     output$sankey.ui <- renderUI({
       links <- sankey$sankey_links
       height <- 500
-      if(!is.null(links)){
-        height <- height + (10*nrow(links))
+      if (!is.null(links)) {
+        height <- height + (10 * nrow(links))
       }
-      plotlyOutput("sankey_plot", height=paste0(height,"px"), width="1500px")
+      plotlyOutput("sankey_plot",
+                   height = paste0(height, "px"),
+                   width = "1300px")
     })
 
     output$sankey_plot <- renderPlotly({
@@ -975,7 +976,7 @@ server <-
               easyClose = TRUE
             )
           )
-        return()
+          return()
         }
         sankey$sankey_links <- tmp$links
         # print(sankey$args_history)
@@ -1047,7 +1048,7 @@ server <-
           mf_tbl <- mf_tbl[, STUDIES]
           mf_tbl <-
             mf_tbl[which(apply(mf_tbl, 1, function(x)
-              ! all(is.na(x)))),]
+              ! all(is.na(x)))), ]
           values$mf_tbl <- mf_tbl
         } else
           withProgress(session = session, value = 0.5, {
@@ -1084,13 +1085,13 @@ server <-
           metafeature <- selected
 
           mf_tbl <- as.data.frame(mf_tbl)
-          abundances <- mf_tbl[metafeature,]
+          abundances <- mf_tbl[metafeature, ]
           abundances <- abundances[which(!is.na(abundances))]
           inds <- which(study_info$study %in% names(abundances))
           df <- study_info[inds, showColumns]
           df$`Relative Abundance` <-
             as.numeric(abundances[study_info$study[inds]])
-          df <- df[order(df$`Relative Abundance`, decreasing = T), ]
+          df <- df[order(df$`Relative Abundance`, decreasing = T),]
 
           output$mfName <-
             renderUI(HTML(
@@ -1142,7 +1143,7 @@ server <-
       isolate(mf_tbl <- as.data.frame(values$mf_tbl))
       selected <- event$curveNumber == 1
       isolate(mf_tbl <-
-                mf_tbl[which(values$mf_selected == selected),])
+                mf_tbl[which(values$mf_selected == selected), ])
 
       row <- event$pointNumber + 1
       metafeature <- rownames(mf_tbl)[row]
@@ -1273,7 +1274,7 @@ server <-
         attribute <- isolate(input$attribute_krona)
         attribute <- ifelse(attribute == "none", "sraID", attribute)
         phylo <- values$phylo
-        tax_table(phylo) <- tax_table(phylo)[, -8]
+        tax_table(phylo) <- tax_table(phylo)[,-8]
         file <- tempfile()
         try(plot_krona(phylo, file, attribute, trim = T))
 
